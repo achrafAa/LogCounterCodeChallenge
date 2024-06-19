3bf4c9bc9ede6233c5fec39c0cc9fbb8bb760d14
achraf <aamri@myswooop.de>
2024-06-19 14:20:26 +0100

    Init Project With Style fixers and dev packages

---
+++ ecs.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSets([
+        SetList::COMMON,
+        SetList::SPACES,
+        SetList::ARRAY,
+        SetList::DOCBLOCK,
+        SetList::NAMESPACES,
+        SetList::COMMENTS,
+        SetList::PSR_12,
+    ]);
---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withSets([
+        SetList::PHP_82,
+        SetList::TYPE_DECLARATION
+    ]);
