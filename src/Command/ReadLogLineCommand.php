35da893ebfce103afcb55f8b1025a59e7ab26bcf
achraf <aamri@myswooop.de>
2024-06-19 16:01:42 +0100

    Create a Command that reads a file in position that is stored in a file for now

---
+++ src/Command/ReadLogLineCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\ReadNextUnprocessedLineService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+#[AsCommand(
+    name: 'log-counter:read-new-line',
+    description: 'Reads new line from log file',
+)]
+class ReadLogLineCommand extends Command
+{
+    private ParameterBagInterface $params;
+    private ReadNextUnprocessedLineService  $readNextUnprocessedLineService;
+
+    public function __construct(
+        ParameterBagInterface $params,
+        ReadNextUnprocessedLineService $readNextUnprocessedLineService
+    )
+    {
+        parent::__construct();
+        $this->params = $params;
+        $this->readNextUnprocessedLineService = $readNextUnprocessedLineService;
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output ): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->info('reading a new line from log file command triggered ');
+
+        $fileName = $this->params->get('env(FILE_NAME)');
+        $filePath = sprintf('%s/var/%s', $this->params->get('kernel.project_dir'), $fileName);
+
+        $positionFileNAME = $this->params->get('env(POSITION_FILE_NAME)');
+        $positionFilePath = sprintf('%s/var/%s', $this->params->get('kernel.project_dir'), $positionFileNAME);
+
+        $io->info('checking file existence ');
+
+        if (!file_exists($filePath)) {
+            $io->error("The file $fileName does not exist.");
+            return Command::FAILURE;
+        }
+
+        $io->info('file found, Getting record ...');
+
+        $line = $this->readNextUnprocessedLineService->read($filePath,$positionFilePath);
+
+        if (!$line){
+            $io->info("no new line found");
+            return Command::SUCCESS;
+        }
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Service/ReadNextUnprocessedLineService.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Service;
+
+use RuntimeException;
+
+class ReadNextUnprocessedLineService
+{
+    /** @var resource|null  */
+    protected $fileHandle = null;
+
+    /**
+     * Reads the file starting from the last processed line pointer position.
+     *
+     * @param string $filePath
+     * @param string $positionFilePath
+     * @return string|null The next unprocessed line or null if no more lines are available.
+     */
+    public function read(string $filePath, string $positionFilePath): ?string
+    {
+        $this->openFile($filePath);
+
+
+        $currentPosition = file_exists($positionFilePath) ? (int) file_get_contents($positionFilePath) : 0;
+        fseek($this->fileHandle, $currentPosition);
+
+        $line = fgets($this->fileHandle);
+        if($line !== false){
+            $lineSize = strlen($line);
+            $nextPosition = $lineSize + $currentPosition;
+            file_put_contents($positionFilePath,$nextPosition);
+        }
+        $this->closeFile();
+        return $line;
+    }
+
+    public function openFile($filePath): void
+    {
+        $this->fileHandle = fopen($filePath, 'r');
+        if (!$this->fileHandle) {
+            throw new RuntimeException('Could not open file "' . $filePath . '"');
+        }
+    }
+
+    public function closeFile(): void{
+        if ($this->fileHandle !== null) {
+            fclose($this->fileHandle);
+            $this->fileHandle = null;
+        }
+    }
+}
