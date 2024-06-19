141e0a23ac0c3d1ca60bf6a071b7ef82828cf246
achraf <aamri@myswooop.de>
2024-06-19 23:02:23 +0100

    add logControllerTest

---
+++ tests/Controller/API/LogControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Tests\Controller\API;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LogControllerTest extends WebTestCase
+{
+    protected function setUp(): void
+    {
+        self::ensureKernelShutdown();
+    }
+
+    public function testCountEndpointRouteExists(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/count');
+
+        $this->assertNotEquals(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testCountEndpointWithoutParams(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/count');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertJson($client->getResponse()->getContent());
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('counter', $responseData);
+        $this->assertIsInt($responseData['counter']);
+
+        $this->assertEquals(20, $responseData['counter']);
+    }
+}
